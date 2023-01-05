811575bb90d6b4d3acd71a8e54633bd4551c67c5
simonho07 <simon.ho07@qq.com>
2023-01-06 02:04:53 +0800

    【支付服务8001】与【订单服务80】入驻【EurekaServer7001】

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description: TODO 类描述
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2023/1/5
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -2,14 +2,19 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description: TODO 类描述
+ * 1.加上 @EnableEurekaClient ，【支付模块8001】要入驻【注册中心EurekaServer7001】
+ * 2.启动服务【注册中心模块EurekaServer7001】、【支付模块8001】
+ * 3.http://localhost:7001/ 可以看到 【支付模块8001】服务
  * @author: hhzh
  * @e-mail: simon.ho07@qq.com
  * @date: 2022/12/14
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
