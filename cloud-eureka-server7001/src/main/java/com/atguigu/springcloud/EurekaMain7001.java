54e92f4360aab3cc9990a3775645a3868c88d3e0
simonho07 <simon.ho07@qq.com>
2023-01-06 01:39:32 +0800

    Eureka Server单机版服务端安装

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description: TODO Eureka Server单机版服务端安装
+ * 1.http://localhost:7001/
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2022/12/15
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
