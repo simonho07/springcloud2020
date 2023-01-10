ee93199ecb3935f538c8c3b873313d0867659cfc
simonho07 <simon.ho07@qq.com>
2023-01-11 01:11:43 +0800

    【gateway配置路由的2种方式】--【1.在配置文件yml中配置  2.代码中注入RouteLocator的Bean】

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GatewayConfig.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/1/11
+ **/
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_atguigu",
+                r -> r.path("/guonei")
+                        .uri("https://news.sina.com.cn/china/")).build();
+        return routes.build();
+    }
+}
