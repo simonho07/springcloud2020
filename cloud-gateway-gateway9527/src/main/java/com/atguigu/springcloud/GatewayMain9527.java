5d6abfe47cda5192d2130563f32097cde7f330d4
simonho07 <simon.ho07@qq.com>
2023-01-11 00:29:29 +0800

    【网关服务gateway9527】搭建 -- 【核心逻辑：路由转发+执行过滤器链】

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GatewayMain9527.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/1/10
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
