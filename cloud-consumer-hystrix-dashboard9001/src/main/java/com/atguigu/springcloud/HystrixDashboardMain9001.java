6b0e86fe32bbe5dc08ef94a12e20aea0a0786b9d
simonho07 <simon.ho07@qq.com>
2023-01-10 13:21:04 +0800

    【Hystrix图形化Dashboard搭建】-- 【cloud-consumer-hystrix-dashboard9001模块】

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/1/10
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
