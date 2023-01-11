a487921b80e9cb08f7c8850e60c6ac054c222b0a
simonho07 <simon.ho07@qq.com>
2023-01-11 11:12:58 +0800

    【gateway常用的predicate】--【 After Route Predicate Factory】

---
+++ cloud-gateway-gateway9527/src/test/java/T.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @description: TODO 打印gateway的after route predicate的时间串
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/1/11
+ **/
+public class T {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();//默认时区
+        System.out.println(now);
+    }
+}
