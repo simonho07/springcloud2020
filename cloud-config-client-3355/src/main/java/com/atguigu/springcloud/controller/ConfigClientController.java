3e2d878eb7023cbfb8805badc6df04168a18c1d3
simonho07 <simon.ho07@qq.com>
2023-01-16 21:52:30 +0800

    【springcloud config 客户端配置】--【模块cloud-config-client-3355】--【动态刷新之手动版，需要使用curl发送post请求，但是否可以通过广播而不是发送post请求解决问题？】

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2023/1/13
  **/
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
