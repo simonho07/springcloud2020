99190333edede0b3b1a6f39a8b0bce0ce568c90e
simonho07 <simon.ho07@qq.com>
2023-02-07 01:24:36 +0800

    【Springcloud Stream 屏蔽消息中间件细节】--【消息驱动之消费者8802】

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/2/7
+ **/
+@Slf4j
+@Controller
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        log.info("消费者1号,----->接受到的消息: " + message.getPayload() + "\t  port: " + serverPort);
+    }
+}
