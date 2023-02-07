c1250aea644933ed59f19831d7a275f3df15ee98
simonho07 <simon.ho07@qq.com>
2023-02-08 00:32:15 +0800

    【Nacos服务配置】--【模块3377】--【修改nacos服务器上配置文件的内容，3377动态刷新配置】

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/2/7
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/2/7
+ **/
+@RestController
+@RefreshScope   //支持Nacos的动态刷新功能
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
