819763757211c9308c675bd49b2cdc325ac4429f
simonho07 <simon.ho07@qq.com>
2023-02-07 19:16:49 +0800

    【Nacos服务注册】--【服务提供者9001、服务提供者9002】

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/2/7
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: simon.ho07@qq.com
+ * @date: 2023/2/7
+ **/
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Long id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}
