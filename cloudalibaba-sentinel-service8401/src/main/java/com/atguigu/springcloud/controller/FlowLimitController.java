47a206a6bbfeefee45d47fe2e8151c0431219cfc
simonho07 <simon.ho07@qq.com>
2023-02-10 22:12:57 +0800

    【Sentinel降级】--【模块8401】--【RT、异常比例、异常数】--【sentinel控制台配置相应的降级规则】

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -25,6 +25,30 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
 
+        log.info(Thread.currentThread().getName() + "\t" + "......testB");
         return "---------------------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+
+        /*try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("------testD测试RT");*/
+
+        log.info("------testD测试 异常比例");
+        int i = 10 / 0;
+        return "---------------------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+
+        log.info("------testD测试 异常数");
+        int i = 10 / 0;
+        return "---------------------testE";
     }
 }
