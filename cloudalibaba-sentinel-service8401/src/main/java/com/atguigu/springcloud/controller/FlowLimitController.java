26d9e82f429eddb8eff7ab4e7695c319df9ea6c6
simonho07 <simon.ho07@qq.com>
2023-02-10 22:42:06 +0800

    【Sentinel热点规则】--【模块8401】--【热点限流、参数例外】--【sentinel控制台配置相应的热点规则】

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.atguigu.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -50,5 +53,18 @@ public class FlowLimitController {
         log.info("------testD测试 异常数");
         int i = 10 / 0;
         return "---------------------testE";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_HotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+
+        return "---------------------testHotKey";
+    }
+
+    public String deal_HotKey(String p1, String p2, BlockException exception) {
+
+        return "---------------------热点数据兜底方法：deal_HotKey";
     }
 }
