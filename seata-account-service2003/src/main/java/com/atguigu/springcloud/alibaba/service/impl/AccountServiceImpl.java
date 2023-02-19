f110c81e749cfdc96d24fe3d7f620079956e3e59
simonho07 <simon.ho07@qq.com>
2023-02-20 00:50:31 +0800

    【Seata分布式事务解决方案】--【模块2001/2002/2003】--【账户余额扣减服务2003模拟超时异常，造成数据库订单未完成、库存已扣减、账户余额已扣减】

--- seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @description: TODO 类描述
@@ -27,6 +28,12 @@ public class AccountServiceImpl implements AccountService {
     public void decrease(Long userId, BigDecimal money) {
 
         LOGGER.info("--------------->seata-account-service扣减账户余额开始");
+        //模拟超时异常，全局事务回滚
+        try {
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         accountDao.decrease(userId, money);
         LOGGER.info("--------------->seata-account-service扣减账户余额end");
     }
